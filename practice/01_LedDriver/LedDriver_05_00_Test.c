49166c6a48f22eb2094507a45e79d40e5c364d51
jinwoki2 <jinwoki2@student.42gyeongsan.kr>
2026-02-07 16:45:30 +0900

    fix(main) : refactoring: delete duplication

--- practice/01_LedDriver/LedDriver_05_00_Test.c
+++ practice/01_LedDriver/LedDriver_05_00_Test.c
@@ -6,7 +6,7 @@
 /*   By: jinwoki2 <jinwoki2@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 23:17:06 by jinwoki2          #+#    #+#             */
-/*   Updated: 2026/02/07 16:36:46 by jinwoki2         ###   ########.fr       */
+/*   Updated: 2026/02/07 16:44:26 by jinwoki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 TEST_GROUP(LedDriver);
 
+static uint16_t	vLeds;
+
 TEST_SETUP(LedDriver)
 {
+	LedDriver_ctor(&vLeds);
 }
 
 TEST_TEAR_DOWN(LedDriver)
@@ -32,16 +35,12 @@ TEST(LedDriver, LedsOffAfterCreate)
 
 TEST(LedDriver, TurnOnLedOne)
 {
-	uint16_t	vLeds;
-	LedDriver_ctor(&vLeds);
 	LedDriver_TurnOn(1);
 	TEST_ASSERT_EQUAL_HEX16(1, vLeds);
 }
 
 TEST(LedDriver, TurnOffLedOne)
 {
-	uint16_t	vLeds;
-	LedDriver_ctor(&vLeds);
 	LedDriver_TurnOn(1);
 	LedDriver_TurnOff(1);
 	TEST_ASSERT_EQUAL_HEX16(0, vLeds);
